64a565420695def6704353e5c93b79dfe9f98763
ruotongyu <ruotongyu@gmail.com>
2019-05-19 10:13:19 +0800

    using port to control chrome

--- main.py
+++ main.py
@@ -3,19 +3,40 @@ import logger
 import logging
 import mycookie
 from selenium import webdriver
+import unittest
+from selenium.webdriver.chrome.options import Options
 
 
 
+class Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        chrome_options = Options()
+        chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+        driver = webdriver.Chrome("./driver/chromedriver.exe", options=chrome_options)
+        logger.Logger().getLog()
+        #changeLessonPage = "https://ke.qq.com/agency/publish/index.html?cid=254956&term_id=100481009&cmd=p"
+        #myCookie = mycookie.MyCookie('./cookie/Cookie.txt')
+        #logging.info("ask changing lesson page：%s" % (changeLessonPage))
+        #driver.get(changeLessonPage)
+        #myCookie.setCookie(driver)
+        cls.driver = driver
 
-if  __name__ == '__main__':
-    # 初始化log
-    logger.Logger().getLog()
-    # 初始化cookie
-    myCookie=mycookie.MyCookie('./cookie/Cookie.txt')
-    driver = webdriver.Chrome("./driver/chromedriver.exe")
-    changeLessonPage ="https://ke.qq.com/agency/publish/index.html?cid=254956&term_id=100481009&cmd=p"
-    driver.get(changeLessonPage)
-    logging.info("ask changing lesson page：%s"%(changeLessonPage))
-    # 设置cookie
-    myCookie.setCookie(driver)
+    @classmethod
+    def tearDownClass(cls):
+        logging.info("执行完所有test")
+
+        #cls.driver.quit()
+    def test_01(cls):
+        print(cls.driver.title)
+        logging.info("进行入test_01")
+
+    def test_02(self):
+        logging.info("进行入test_02")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
 
